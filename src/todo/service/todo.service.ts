80bc18889e5c237e3cbb1fd0932874c1a36a8d49
JMyang30 <didwoans30@gmail.com>
2024-03-13 18:38:59 +0900

    Revert "FEAT: TODO CRUD 기능 추가"
    This reverts commit df0e20de5f83fbc5994196803475c9c853f829b7.

--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -10,44 +10,4 @@ export class TodoService {
   async fetchAllTodos(): Promise<Todo[]> {
     return this.prismaService.todo.findMany();
   }
-
-  // 단일 조회
-  async fetchTodoItem(id: number): Promise<Todo | null> {
-    return this.prismaService.todo.findUnique({
-      where: { id: Number(id) },
-    });
-  }
-
-  // 단일 삭제
-  async deleteTodoItem(id: number): Promise<Todo | null> {
-    return this.prismaService.todo.delete({
-      where: {
-        id: Number(id),
-      },
-    });
-  }
-
-  // 단일 수정
-  async updateTodoItem(
-    id: number,
-    title: string,
-    content: string,
-    is_done: boolean,
-  ): Promise<Todo | null> {
-    return this.prismaService.todo.update({
-      where: {
-        id: Number(id),
-      },
-      data: {
-        title,
-        content,
-        is_done,
-      },
-    });
-  }
-
-  // 단일 추가
-  async addTodoItem(data: Todo): Promise<Todo | null> {
-    return this.prismaService.todo.create({ data: data });
-  }
 }
